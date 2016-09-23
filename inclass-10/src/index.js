9c1ae33f54f88fb03a9b33838d24ddc4e646b473
hedai <daihe1992@gmail.com>
2016-09-22 23:09:24 -0500

    inclass10

---
+++ inclass-10/src/index.js
@@ -0,0 +1,101 @@
+//
+// Inclass React ToDo Exercise
+// ============================
+//
+// Using the views as described in our previous exercise
+// re-implement the ToDo App using React.
+// 
+// Below you will transpile the h() function calls
+// into JSX and implement ToDos.addTodo()
+//
+;(function() {
+
+'use strict'
+
+class ToDoItem extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            done: false
+        }
+    }
+
+    render() { return (
+        <li>
+            <i className="check glyphicon glyphicon-check" onClick={()=>this.setState({done:!this.state.done})}></i>
+            <span className={this.state.done ? "completed" : ""}> {typeof(this.props.text) === "string" ? this.props.text : ""}</span>
+            <i className="destroy glyphicon glyphicon-remove" onClick={this.props.remove}></i>
+        </li>
+        /*
+        h("li", { id: `task${_taskId++}`}, [
+            h("i", { className: "check glyphicon glyphicon-check", onClick: toggleDone }, []),
+            h("span", { contentEditable: true, done: false }, typeof(text) === "string" ? text : ""),
+            h("i", { className: "destroy glyphicon glyphicon-remove", onClick: removeTask }, []),
+        ])
+        */
+    )}
+}
+
+class ToDos extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.nextId = 2;
+        this.state = {
+            todoItems: [
+                {id:0, text:"This is an item"},
+                {id:1, text:"Another item" }
+            ]
+        }
+    }
+
+    addTodo() {
+        // IMPLEMENT ME!
+        const text = document.getElementById('newTODO').value
+            if(text != ""){             
+                this.setState({ todoItems: [
+                        ...this.state.todoItems, 
+                        {id:this.nextId++, text}
+                    ]
+                })
+            }   
+        document.getElementById('newTODO').value=""
+    }
+
+    removeTodo(removeId) {
+        this.setState({ 
+            todoItems: this.state.todoItems.filter(({id, text}) => id != removeId)
+        })
+    }
+
+    render() { return (
+        <div>
+            <input id="newTODO" type="text" placeholder="To Do"></input>
+            <button onClick={()=>this.addTodo()}>Add Item</button>
+            <span className="submit">
+                <a href="https://webdev-rice.herokuapp.com" target="_blank">Submit your exercise</a>
+            </span>
+            <ul className="todo">
+                {this.state.todoItems.map((i) =>
+                    <ToDoItem key={i.id} text={i.text} remove={() => this.removeTodo(i.id)} />)
+                }
+            </ul>
+        </div>
+        /*
+        h("div", { },
+            h("input", { id: "newTODO", type: "text", placeholder: "To Do"}),
+            h("button", { onClick: addItem }, "Add Item"),
+            h("span", { className: "submit" }, [
+                h("a", { href: "https://webdev-rice.herokuapp.com",
+                     target: "_blank" }, "Submit your exercise"),
+            ]),
+            h("ul", { className: "todo" }, listItems)
+        )
+        */
+    )}
+}
+
+ReactDOM.render(<ToDos/>, document.getElementById('app'));
+
+})()
